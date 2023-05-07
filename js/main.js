b459711bfbd35e1f29c40ec3e21aaca698c67274
Eldar <eldar.akhmetshin.93@mail.ru>
2023-05-07 11:17:24 +0400

    блокировка и разблокировка формы и фильтров

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {createAnnouncementObjects} from './data.js';
 import {renderAllCards} from './thumbnails.js';
+import {switchNotActiveState} from './form_states.js';
 
 const createdAnnouncementObjects = createAnnouncementObjects();
 renderAllCards(createdAnnouncementObjects);
+switchNotActiveState();
