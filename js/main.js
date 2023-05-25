7850930e1a116f622d9ff3790f04dce34af95a39
Eldar <eldar.akhmetshin.93@mail.ru>
2023-05-25 20:52:22 +0400

    исправил ошибку

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import {createAnnouncementObjects} from './data.js';
 import {renderAllCards} from './thumbnails.js';
-import {setStateNotActive} from './form_states.js';
 import {checkNewForm, addNewFormListeners} from './validation_new_form.js';
 import {renderMap} from './map.js';
 
